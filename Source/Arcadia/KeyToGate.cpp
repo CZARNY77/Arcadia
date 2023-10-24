5827e56e188ac89fe6a1813f48b0df443c3aa881
Jakub <60876248+CZARNY77@users.noreply.github.com>
2023-10-24 23:56:19 +0200

    13 - Key

---
+++ Source/Arcadia/KeyToGate.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KeyToGate.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AKeyToGate::AKeyToGate()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	KeyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("KeyMesh"));
+	RootComponent = KeyMesh;
+
+	SphereCollider = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollider"));
+	SphereCollider->InitSphereRadius(10.f);
+	SphereCollider->SetCollisionProfileName("Trigger");
+	SphereCollider->SetupAttachment(KeyMesh);
+
+}
+
+// Called when the game starts or when spawned
+void AKeyToGate::BeginPlay()
+{
+	Super::BeginPlay();
+	SphereCollider->OnComponentBeginOverlap.AddDynamic(this, &AKeyToGate::OnOverlapBox);
+}
+
+// Called every frame
+void AKeyToGate::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AKeyToGate::OnOverlapBox(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
---
+++ Source/Arcadia/KeyToGate.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "KeyToGate.generated.h"
+
+UCLASS()
+class ARCADIA_API AKeyToGate : public AActor
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Key Settings")
+	class UStaticMeshComponent* KeyMesh;
+	UPROPERTY(EditAnywhere, Category = "Key Settings")
+	class USphereComponent* SphereCollider;
+
+public:	
+	// Sets default values for this actor's properties
+	AKeyToGate();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+	void OnOverlapBox(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
